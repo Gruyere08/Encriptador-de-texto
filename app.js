d4cb1eca62476385d28defb265734e1f4144f84d
Jacob <athiaguin.es@gmail.com>
2024-07-12 23:59:41 -0300

    Agregado boton reiniciar y pantalla de error

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-
-
-
-
+const a = 'ai';
+const e = 'enter';
+const li = 'imes';
+const o = 'ober';
+const u = 'ufat';
 
 function encriptarTexto(texto) {
+
+    if (ilegal(texto)) {
+        rechazar();
+        return
+    }
     
-    primerResultado();
+    if (texto !== "") {
+        ocultarCartel();
+    }
+
 
     let resultado = "";
 
@@ -14,19 +23,19 @@ function encriptarTexto(texto) {
 
         switch (char) {
             case 'a':
-                resultado += 'ai';
+                resultado += a;
                 break;
             case 'e':
-                resultado += 'enter';
+                resultado += e;
                 break;
             case 'i':
-                resultado += 'imes';
+                resultado += li;
                 break;
             case 'o':
-                resultado += 'ober';
+                resultado += o;
                 break;
             case 'u':
-                resultado += 'ufat';
+                resultado += u;
                 break;
             default:
                 resultado += char;
@@ -42,15 +51,50 @@ function encriptarTexto(texto) {
 function encriptar() {
     let texto = document.getElementById("campoEntrada").value
     document.getElementById("campoSalida").value = encriptarTexto(texto);
+    
+    return;
 };
 
+function rechazar() {
+    let parrafo = document.getElementById('parrafoResultado');
+    let titulo = document.getElementById('tituloResultado');
+    document.getElementById('imagenInicial').style.display = "none";
+    document.getElementById('copiar').style.visibility = "hidden";
+    document.getElementById('campoSalida').style.display = "none";
+    document.getElementById('imagenError').style.display = "inline";
+    parrafo.style.display = "block";
+    titulo.style.display = "block";
+    parrafo.innerHTML = "No se permiten <strong>mayusculas</strong> ni <strong>tildes</strong>."
+    titulo.textContent = "Algo anduvo mal."
+    
+}
 
-function primerResultado(){
-    document.getElementById('copiar').style.visibility = "visible";
-    document.getElementById('campoSalida').style.display = "block";
+
+function ocultarCartel(){
+    mostrarResultado();
+    document.getElementById('imagenError').style.display = "none";
     document.getElementById('imagenInicial').style.display = "none";
     document.getElementById('parrafoResultado').style.display = "none";
     document.getElementById('tituloResultado').style.display = "none";
 
-    
+    return;
+}
+
+
+function mostrarResultado(){
+    document.getElementById('copiar').style.visibility = "visible";
+    document.getElementById('campoSalida').style.display = "block";
+
+    return;
+}
+
+
+function ilegal(str) {
+    const regular = /[A-ZÁÉÍÓÚÜáéíóú]/;
+    return regular.test(str);
+}
+
+function limpiar() {
+    document.getElementById('campoEntrada').value = "";
+    return;
 }
