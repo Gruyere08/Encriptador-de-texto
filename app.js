d15f2696d2227d582a8d64c49bcc333b22efe23d
Jacob <athiaguin.es@gmail.com>
2024-07-12 21:46:43 -0300

    Agregada pantalla inicial

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 
 
 function encriptarTexto(texto) {
+    
+    primerResultado();
+
     let resultado = "";
 
     for (let i = 0; i < texto.length; i++) {
@@ -40,3 +43,14 @@ function encriptar() {
     let texto = document.getElementById("campoEntrada").value
     document.getElementById("campoSalida").value = encriptarTexto(texto);
 };
+
+
+function primerResultado(){
+    document.getElementById('copiar').style.visibility = "visible";
+    document.getElementById('campoSalida').style.display = "block";
+    document.getElementById('imagenInicial').style.display = "none";
+    document.getElementById('parrafoResultado').style.display = "none";
+    document.getElementById('tituloResultado').style.display = "none";
+
+    
+}
