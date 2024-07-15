3604ba19dd17ccd46e91dab049dedaa1f880ecbc
Jacob <athiaguin.es@gmail.com>
2024-07-15 13:58:48 -0300

    Terminado transicion de tema y actualizado iconos

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const li = 'imes';
 const o = 'ober';
 const u = 'ufat';
 
+const cartel = ['imagenError', 'imagenInicial', 'parrafoResultado', 'tituloResultado']
+const iconos = ["Alura", "warning", "imagenError", "imagenInicial", "icono_tema", "icono_home", "icono_info"];
+const bloques = ["bloque_principal", "bloque_secundario", "bloque_botones", "titulo"]
+const boton_tema = document.getElementById("boton_tema");
+
+
 const temaGuardado = localStorage.getItem('tema') || 'oscuro';
 document.documentElement.setAttribute('tema', temaGuardado);
-let iconos = ["Alura", "warning", "imagenError", "imagenInicial", "icono_tema"];
-let bloques = ["bloque_principal", "bloque_secundario", "bloque_botones"]
+boton_tema.addEventListener("click", cambiarTema);
 actualizarIconos();
 transicionBloques();
+aparecerIconos();
+
+
+
 
 function encriptarTexto(texto) {
 
@@ -17,7 +26,7 @@ function encriptarTexto(texto) {
         rechazar();
         return
     }
-    
+
     if (texto !== "") {
         ocultarCartel();
     }
@@ -53,12 +62,29 @@ function encriptarTexto(texto) {
 }
 
 
+function esconderIconos() {
+    let botones = document.querySelectorAll(".menu__boton");
+    for (const elemento of botones) {
+        elemento.classList.remove("fuera_de_pantalla");
+        elemento.classList.remove("aparecer_por_izquierda");
+        elemento.classList.add("salir_por_izquierda");
+    }
+}
+
+function aparecerIconos() {
+    let botones = document.querySelectorAll(".menu__boton");
+    for (const elemento of botones) {
+        elemento.classList.add("fuera_de_pantalla");
+        elemento.classList.remove("salir_por_izquierda");
+        elemento.classList.add("aparecer_por_izquierda");
+    }
+}
 
 
 function encriptar() {
     let texto = document.getElementById("campoEntrada").value
     document.getElementById("campoSalida").value = encriptarTexto(texto);
-    
+
     return;
 };
 
@@ -73,24 +99,28 @@ function rechazar() {
     titulo.style.display = "block";
     parrafo.innerHTML = "No se permiten <strong>mayusculas</strong> ni <strong>tildes</strong>."
     titulo.textContent = "Algo anduvo mal."
-    
+
 }
 
 
-function ocultarCartel(){
-    mostrarResultado();
-    document.getElementById('imagenError').style.display = "none";
-    document.getElementById('imagenInicial').style.display = "none";
-    document.getElementById('parrafoResultado').style.display = "none";
-    document.getElementById('tituloResultado').style.display = "none";
-
+function ocultarCartel() {
+    
+    for (const elemento of cartel) {
+        document.getElementById(elemento).classList.add("oculto")
+        setTimeout(() => {
+            document.getElementById(elemento).style.display = "none";
+            mostrarResultado();
+        }, 300);
+    }
     return;
 }
 
 
-function mostrarResultado(){
+function mostrarResultado() {
     document.getElementById('copiar').style.visibility = "visible";
     document.getElementById('campoSalida').style.display = "block";
+    document.getElementById('copiar').classList.remove("oculto")
+    document.getElementById('campoSalida').classList.remove("oculto")
 
     return;
 }
@@ -108,36 +138,41 @@ function limpiar() {
 
 
 function cambiarTema() {
-    
+    boton_tema.disabled = true;
     transicionBloques();
+    esconderIconos();
     const temaActual = document.documentElement.getAttribute('tema');
     const temaNuevo = temaActual === 'oscuro' ? 'claro' : 'oscuro';
     setTimeout(() => {
         document.documentElement.setAttribute('tema', temaNuevo);
         actualizarIconos();
         transicionBloques();
-    }, 1100); 
+        aparecerIconos();
+        boton_tema.disabled = false;
+    }, 1100);
     localStorage.setItem('tema', temaNuevo);
     return;
 }
 
 
-function actualizarIconos(){
+function actualizarIconos() {
     if (oscuro()) {
         for (let icono of iconos) {
             oscurecer(document.getElementById(icono));
+            boton_tema.setAttribute("texto-cartel", "Tema claro")
         }
     } else {
         for (let icono of iconos) {
             enclarecer(document.getElementById(icono));
+            boton_tema.setAttribute("texto-cartel", "Tema oscuro")
         }
     }
-    
+
     return 0;
 }
 
 
-function oscuro(){
+function oscuro() {
     return document.documentElement.getAttribute('tema') === "oscuro";
 }
 
@@ -154,7 +189,7 @@ function oscurecer(elemento) {
 }
 
 
-function transicionBloques(){
+function transicionBloques() {
     console.log("entre al transition");
     for (const elemento of bloques) {
         document.getElementById(elemento).classList.toggle("oculto");
